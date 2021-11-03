489486ceff6a3322e340a43a2d76be8c24eb624c
omarturmun <93262369+omarturmun@users.noreply.github.com>
2021-11-03 14:05:43 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,43 @@
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (input.lightLevel() > 90) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (input.lightLevel() > 80) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    } else if (input.lightLevel() > 70) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . # . .
+            . # . . .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+})
